1c6640567631d7af7b1ded4170c3f6866c6cbfed
nsdubey <ndubey@techferry.com>
2020-06-22 12:42:38 +0530

    Updates

---
+++ precache-manifest.77ad52cc3a23a90283c35cd50d616252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe6db44fb59f2b9921d1",
+    "url": "/comedyMovies/static/css/main.46ba7efb.chunk.css"
+  },
+  {
+    "revision": "fe6db44fb59f2b9921d1",
+    "url": "/comedyMovies/static/js/main.b8375351.chunk.js"
+  },
+  {
+    "revision": "6dc4ef6ba33c700a3a85",
+    "url": "/comedyMovies/static/js/runtime~main.08c70866.js"
+  },
+  {
+    "revision": "0abc8a19e39facb79eef",
+    "url": "/comedyMovies/static/js/2.cdc66a79.chunk.js"
+  },
+  {
+    "revision": "8891b554687bac723c5e464e15665d8c",
+    "url": "/comedyMovies/index.html"
+  }
+];
